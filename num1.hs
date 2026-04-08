b55ab18b627af82c53f5fbe076d5193cb44ffb08
Kevin Ntumi <34671981+kevinntumi@users.noreply.github.com>
2026-04-08 18:47:44 +0200

    Update num1.hs

--- num1.hs
+++ num1.hs
@@ -1,4 +1,5 @@
 import Data.Char (isNumber)
+import Control.Monad (when)
 
 fatorial :: Integer -> Integer
 fatorial 0 = 1
@@ -16,6 +17,14 @@ numTelefone ('8':d:_)
     | otherwise     = "Número de telefone de operadora desconhecida"
 numTelefone _ = "Número de telefone de operadora estrangeira"
 
+nEsimoCodigoEstudanteSIGA :: Int -> [Integer]
+nEsimoCodigoEstudanteSIGA n
+    | n <= 0 = error "O número deve ser maior que zero."
+    | otherwise = take n codigoEstudantesSIGA
+
+codigoEstudantesSIGA :: [Integer]
+codigoEstudantesSIGA = [20230000..]
+
 saoDigitos :: String -> Bool
 saoDigitos = all isNumber
 
@@ -29,7 +38,14 @@ percorrerLista (x:xs) = do
     percorrerLista xs
 
 opcoes :: [String]
-opcoes = ["1. Imprimir fatorial", "2."]
+opcoes = 
+    [
+        "1. Imprimir o n-ésimo fatorial", 
+        "2. Identificar operadora de número de telefones",
+        "3. Gerar os n primeiros códigos de estudantes do SIGA",
+        "4. ",
+        "7. Sair"
+    ]
 
 foiAceiteResposta :: String -> Bool
 foiAceiteResposta [] = False
@@ -39,6 +55,43 @@ mostrarMenu :: IO ()
 mostrarMenu = do
     putStrLn "Bem vindo ao menu:"
     percorrerLista opcoes
+    opcaoStr <- getLine
+    let opcao = read opcaoStr :: Integer
+
+    case opcao of
+        1 -> 
+            do
+                putStrLn "Digite um número para calcular o fatorial:"
+                numStr <- getLine
+
+                if all isNumber numStr then 
+                    do
+                        let n = read numStr :: Integer
+                        putStrLn $ "O fatorial de " ++ show n ++ " é: " ++ show (fatorial n)
+                else 
+                    putStrLn "Entrada inválida. Por favor, digite um número inteiro."
+        2 -> 
+            do
+                putStrLn "Digite um número de telefone para obter a operadora:"
+                numTelStr <- getLine
+                putStrLn $ numTelefone numTelStr
+        3 -> 
+            do
+                putStrLn "Digite o número de códigos de estudantes do SIGA que deseja imprimir:"
+                nEsimoStr <- getLine
+
+                if all isNumber nEsimoStr then 
+                    do
+                        let n = read nEsimoStr :: Int
+                        putStrLn $ "Os " ++ show n ++ " primeiros códigos de estudantes SIGA são: " ++ show (nEsimoCodigoEstudanteSIGA n)
+                else 
+                    putStrLn "Entrada inválida. Por favor, digite um número inteiro."
+        7 -> 
+            return ()
+        _ -> 
+            do
+                putStrLn "Opção inválida. Por favor, escolha uma opção válida."
+                mostrarMenu
 
     putStrLn "\nDeseja voltar ao menu principal? (s/n)"
     resposta <- getLine
